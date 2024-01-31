19a47119fe87cfb1d930226f273a497ec17faacb
mohammed younis <mohammedyounis1996@gmail.com>
2024-01-30 22:37:31 -0600

    second commit

---
+++ app.py
@@ -0,0 +1,154 @@
+#field to insert Job Description
+#Upload resume PDF
+#Convert PDF to image --> processing --> Google Gemini Pro
+from dotenv import load_dotenv
+
+load_dotenv()
+import base64
+import streamlit as st
+import os
+import io
+from PIL import Image 
+import pdf2image
+import google.generativeai as genai
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input,pdf_content,prompt):
+    model=genai.GenerativeModel("gemini-pro-vision")
+    response=model.generate_content([input,pdf_content[0],prompt])
+    return response.text
+#convert the pdf to image
+def input_pdf_setup(uploaded_file):
+    if uploaded_file is not None:
+        ## Convert the PDF to image
+        images=pdf2image.convert_from_bytes(uploaded_file.read())
+
+        first_page=images[0]
+
+        # Convert to bytes
+        img_byte_arr = io.BytesIO()
+        first_page.save(img_byte_arr, format='JPEG')
+        img_byte_arr = img_byte_arr.getvalue()
+
+        pdf_parts = [
+            {
+                "mime_type": "image/jpeg",
+                "data": base64.b64encode(img_byte_arr).decode()  # encode to base64
+            }
+        ]
+        return pdf_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+#streamlit app
+st.set_page_config(page_title="Mohammed's ATS Resume Analysis")
+st.header("Resume Analysis System by Mohammed Younis")
+input_text=st.text_area("Job Description:",key="input")
+uploaded_file=st.file_uploader("Upload your resume in PDF Format", type=["pdf"])
+if uploaded_file is not None:
+    st.write("PDF was uploaded successfully")
+submit1=st.button("Tell Me About the Resume")
+submit2=st.button("What are the Keywords for this Job")
+submit3 = st.button("How Can I Improve my Skills")
+submit4 = st.button("Percentage Match between the Resume and the Job Description")
+submit5 = st.button("Write a Cover Letter for this job")
+submit6 = st.button("Ask me an Interview Question Using this Job Description?")
+submit7 = st.button("Does the Resume Contain Any Grammatical or Spelling Errors? or any formatting issues")
+Additional=st.text_input("Additional Questions?")
+
+input_prompt1 = """
+ You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
+  Please share your professional evaluation on whether the candidate's profile aligns with the role. 
+ Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+"""
+input_prompt2 = """
+ You are an experienced Technical Human Resource Manager,your task is to review the provided job description. 
+  Please share the keywords that are in this job description.
+"""
+input_prompt3 = """
+ You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
+  Please share your professional evaluation on how the candidate can improve his resume to make it more suitable for the job requirements.
+"""
+
+input_prompt4 = """
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
+your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
+the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+"""
+
+input_prompt5 = """
+You are an experienced Technical Human Resource Manager,your task is to review the provided resume and the job description. 
+  Please write a cover letter for this job using the provided resume.
+"""
+input_prompt6= """
+You are an experienced Technical Human Resource Manager,your task is to review the provided resume and the job description. 
+  Please ask me interview question using both the job description and the provided resume?
+"""
+input_prompt7= """
+You are an experienced Technical Human Resource Manager,your task is to review the provided resume and check if the resume contains any grammatical or spelling errors, or formatting issues.
+"""
+
+if submit1:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt1,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please upload the resume")
+elif submit2:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt2,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please upload the resume")
+elif submit3:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt3,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please upload the resume")
+elif submit4:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt4,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please upload the resume")
+elif submit5:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt5,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please upload the resume")
+elif submit6:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt6,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please upload the resume")
+elif submit7:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt7,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please upload the resume")
+elif Additional:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(Additional,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please upload the resume")
